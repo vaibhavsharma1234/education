cf6ffe19909146e2cf09186eb43d2ccd1de0827f
DeepakS-Github <deepakanonymous75@gmail.com>
2023-02-05 19:47:50 +0530

    Created some UI components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import AndroidApp from './components/AndroidApp'
 import Navbar from './components/Navbar'
+import OurResults from './components/OurResults'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <AndroidApp/>
+      <OurResults/>
     </div>
   )
 }
