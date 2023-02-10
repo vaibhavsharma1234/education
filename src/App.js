606f9f9701f8d97ee4e204f079754dbd344093db
vaibhav <vaibh>
2023-02-10 19:00:59 +0530

    new 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Navbar from './components/Navbar'
-
+import Home from './components/hero/Home'
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   )
 }
