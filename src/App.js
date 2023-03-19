a83630daad1c94e813e43e275eecdfe8e4c51d42
DeepakS-Github <deepakanonymous75@gmail.com>
2023-03-20 02:01:44 +0530

    Linking is left

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import AndroidApp from './components/AndroidApp'
 import Navbar from './components/Navbar'
-import OurResults from './components/OurResults'
-import OfflineCenter from './components/OfflineCenter'
-import Table from './components/Table'
+import React from 'react'
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './components/Home';
+import CoursesFor10th from './components/CoursesFor10th';
+import CousesForOthersInBoards from './components/CousesForOthersInBoards';
+import Chapters from './components/Chapters';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <AndroidApp/>
-      <OurResults/>
-      <OfflineCenter/>
-      <Table/>
-      
-    </div>
+    <>
+    {/* <Navbar /> */}
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/courses" element={<CoursesFor10th/>}/>
+        <Route exact path="/coursesfor" element={<CousesForOthersInBoards/>}/>
+        <Route exact path="/chapters" element={<Chapters/>}/>
+      </Routes>
+    </Router>
+    </>
+
   )
 }
 
